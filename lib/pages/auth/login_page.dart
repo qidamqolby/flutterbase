431d8dae1308855e0388f397a1abfc7bb5bbb817
Qidam <qidam.qolby@missiidea.com>
2023-02-28 23:17:36 +0700

    update login page

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -37,7 +37,29 @@ class _LoginPageState extends State<LoginPage> {
                       color: Colors.grey[200],
                       border: Border.all(color: Colors.white),
                       borderRadius: BorderRadius.circular(12)),
-                  child: TextField(),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Email')),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            border: InputBorder.none, hintText: 'Password')),
+                  ),
                 ),
               )
             ],
